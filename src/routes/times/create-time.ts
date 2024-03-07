88a5c79b748d5cd24e54f2b01b070608bd1bbdd0
ViniGhiraldi <vinighiraldi198@gmail.com>
2024-03-06 22:35:23 -0300

    feat: added multer lib and create "time" route

--- src/routes/times/create-time.ts
+++ src/routes/times/create-time.ts
@@ -1,17 +1,74 @@
 import { FastifyInstance } from "fastify";
 import z from "zod";
+import upload from "../../lib/multer";
+import { prisma } from "../../lib/prisma";
+
+interface FileType {
+    filename: string;
+    path: string;
+    size: number;
+}
 
 export async function createTime(app: FastifyInstance) {
-    app.post('/time/:rachaoId', async (req, res) => {
+    app.post('/time/:rachaoId', { preHandler: upload.single('imagem') }, async (req, res) => {
         const paramsValidation = z.object({
             rachaoId: z.string().cuid()
         })
-
+        
         const bodyValidation = z.object({
             nome: z.string().trim()
         })
-
+        
         const { rachaoId } = paramsValidation.parse(req.params);
         const { nome } = bodyValidation.parse(req.body);
+        const file = (req as any).file as FileType;
+
+        try {
+            let result;
+            if(file){
+                result = await prisma.times.create({
+                    data: {
+                        nome: nome,
+                        rachaoId: rachaoId,
+                        imagem: {
+                            create: {
+                                name: file.filename,
+                                path: file.path,
+                                size: file.size,
+                                url: `${process.env.IMAGES_URL}/${file.filename}`
+                            }
+                        }
+                    },
+                    select: {
+                        id: true,
+                        nome: true,
+                        imagem: {
+                            select: {
+                                id: true,
+                                name: true,
+                                path: true,
+                                size: true,
+                                url: true
+                            }
+                        }
+                    }
+                })
+            }else{
+                result = await prisma.times.create({
+                    data: {
+                        nome: nome,
+                        rachaoId: rachaoId
+                    },
+                    select: {
+                        id: true,
+                        nome: true
+                    }
+                })
+            }
+
+            return res.status(201).send({data: result});
+        } catch (error) {
+            return res.status(500).send({error});
+        }
     })
 }
--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,19 @@ import { putRachao } from "./routes/rachao/put-rachao";
 import { deleteRachao } from "./routes/rachao/delete-rachao";
 import { getFullRachao } from "./routes/rachao/get-full-rachao";
 import { getAllRachao } from "./routes/rachao/get-all-rachao";
+import multer from "fastify-multer";
+import { createTime } from "./routes/times/create-time";
 
 const server = fastify();
+
+server.register(multer.contentParser);
 
 server.register(createRachao);
 server.register(putRachao);
 server.register(deleteRachao);
 server.register(getFullRachao);
 server.register(getAllRachao);
+
+server.register(createTime);
 
 server.listen({ port: Number(process.env.PORT) || 3333 }, () => console.log(new Date()))
